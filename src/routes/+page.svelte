84cc7bcf762ac2e6c1f1733d3a0162a58133b993
teo-clone <theodore.tsivranidis@gmail.com>
2024-09-27 00:41:52 -0400

    small revamp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
-<div class="flex flex-col items-center gap-10 py-10">
+<div class="flex flex-col items-center gap-10 py-10 text-2xl">
 	<img src="/skater.gif" alt="skater gif" class="py-16 w-[10rem] mr-[3.5rem]" />
 
-	<a href="/fireworks" class="underline text-blue-600">fireworks</a>
+	<a href="/fireworks" class=" rotate-[17deg] underline text-blue-600 hover:text-gray-200"
+		>fireworks</a
+	>
+
+	<a
+		href="https://teo.fyi/cc/coins"
+		class="rotate-[-10deg] underline text-blue-600 hover:text-gray-200">coins</a
+	>
+
+	<a
+		href="https://teo.fyi/cc/face"
+		class="rotate-[-2deg] underline text-blue-600 hover:text-gray-200">face</a
+	>
+
+	<a
+		href="https://teo.fyi/cc/mouse"
+		class="rotate-[7deg] underline text-blue-600 hover:text-gray-200">mouse</a
+	>
 </div>
