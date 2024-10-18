0d33aa10e415854c1de8492fabf03f58de3ecc78
teo-clone <theodore.tsivranidis@gmail.com>
2024-10-18 02:05:19 -0400

    falling text hallelujah

--- src/routes/impermanence/+page.svelte
+++ src/routes/impermanence/+page.svelte
@@ -3,27 +3,47 @@
 	import gsap from 'gsap';
 	import { onMount } from 'svelte';
 
-	let letters;
 	onMount(() => {
-		const example = new Letterize({
+		const handleHover = (event) => {
+			const target = event.target;
+
+			if (event.type === 'mouseenter') {
+				scale(target);
+			}
+		};
+
+		const text = new Letterize({
 			targets: '#letterize p'
 		});
 
-		letters = example.listAll;
+		const letters = text.listAll;
 
-		const scale = (element: Element, s: number) => {
-			gsap.to(element, {
-				scale: s,
-				duration: 0.3,
+		letters.forEach((span) => {
+			span.style.display = 'inline-block';
+			span.addEventListener('mouseenter', handleHover);
+			span.addEventListener('mouseleave', handleHover);
+		});
+
+		const scale = (e: Element) => {
+			const windowHeight = window.innerHeight;
+			const elementRect = e.getBoundingClientRect();
+			const distanceToBottom = windowHeight - elementRect.bottom;
+
+			gsap.to(e, {
+				color: 'green',
+				duration: 0.5,
+				yoyo: true,
 				ease: 'power1.inOut'
 			});
-		};
 
-		letters.forEach((letter) => {
-			if (!letter.textContent || letter.textContent.trim() === '') return;
-			letter.addEventListener('mouseenter', () => scale(letter, 0));
-			letter.addEventListener('mouseleave', () => scale(letter, 1));
-		});
+			// Create a timeline for the falling effect
+			var tl = gsap.timeline({ delay: Math.random() * 3 });
+			tl.to(e, {
+				duration: 4,
+				y: `+=${distanceToBottom}`, // Move the element to the bottom dynamically
+				ease: 'bounce.out'
+			});
+		};
 	});
 </script>
 
