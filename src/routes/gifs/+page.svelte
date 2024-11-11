e18e36adb637fa090bdc44932b7cc98c1f550039
teo-clone <theodore.tsivranidis@gmail.com>
2024-11-11 00:23:35 -0500

    remove sobu from play

---
+++ src/routes/gifs/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col sm:flex-row min-h-screen justify-center items-center gap-5">
+	<img src="/bike.gif" alt="skater gif" class="max-w-[40rem]" />
+	<img src="/walk.gif" alt="skater gif" class="max-w-[31rem]" />
+</div>
